58824bdc906c9f0861a073d0b05da35e268c1a33
Mitchell Zhou <mz2909@nyu.edu>
2023-10-03 20:00:28 -0400

    sql uploaded

--- init.py
+++ init.py
@@ -7,12 +7,16 @@ import string, random
 def index():
     numbers = [('00', 'green'), ('27', 'red'), ('10', 'black'), ('35', 'red'), ('29', 'black'), ('12', 'red'), ('8', 'black'), ('19', 'red'),
                ('31', 'black'), ('18', 'red'), ('6', 'black'), ('21', 'red'), ('33', 'black'), ('16', 'red'), ('4', 'black'), ('23', 'red'),
-               ('35', 'black'), ('14', 'red'), ('2', 'black'), ('0', 'red'), ('28', 'black'), ('9', 'red'), ('26', 'black'), ('30', 'red'),
+               ('35', 'black'), ('14', 'red'), ('2', 'black'), ('0', 'green'), ('28', 'black'), ('9', 'red'), ('26', 'black'), ('30', 'red'),
                ('11', 'black'), ('7', 'red'), ('20', 'black'), ('32', 'red'), ('17', 'black'), ('5', 'red'), ('22', 'black'), ('34', 'red'),
-               ('15', 'black'), ('3', 'red'), ('24', 'black'), ('36', 'red'), ('13', 'black'), ('1', 'red'), ('00', 'green')]
-
+               ('15', 'black'), ('3', 'red'), ('24', 'black'), ('36', 'red'), ('13', 'black'), ('1', 'red')]
 
     res = make_response(render_template('index.html'))
+    turn = len(numbers) / 360
+    pointer = 0
+
+    ans = random.randrange(0, 39)
+    
     return res
 
 
